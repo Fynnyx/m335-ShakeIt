0d75366465e468c35c6129356454bcdcc676da93
Fynn Westerath <westerathf@gmail.com>
2023-01-18 09:23:19 +0100

    Layout done, Basic shake detection

--- app/src/main/java/ch/fwesterath/shakeit/MainActivity.java
+++ app/src/main/java/ch/fwesterath/shakeit/MainActivity.java
@@ -8,6 +8,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,12 +31,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private final static int DISPLAY_PERIOD = 30; // in seconds
     private final static int CHART_ENTRIES_LIMIT = SAMPLING_RATE / 100000 * DISPLAY_PERIOD; // equals 300 displayed entries
 
-    private int SHAKE_INTESITY = 20;
+    private final int SHAKE_INTESITY = 40;
+    private final int MIN_SHAKE_INTESITY = 10;
+
 
     // Shaking Status
     private boolean isShaking = false;
     // High Score
-    private float highscore = 0;
+    private int highscore = 0;
+    // Current Score
+    private int score = 0;
 
     // Shared Preferences
     private SharedPreferences sharedPreferences;
@@ -71,6 +77,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         highscoreTextView = findViewById(R.id.highscoreValue);
         scoreTextView = findViewById(R.id.scoreValue);
 
+        // Setup Buttons
+        Button resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(v -> {
+            resetHighscore();
+        });
+
+
+
+
         // Set chart and AxisLine styling
         chart.setBackgroundColor(getResources().getColor(R.color.white));
 
@@ -91,11 +106,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sharedPreferences = getSharedPreferences("SHARED_PREFS", MODE_PRIVATE);
         highscore = sharedPreferences.getInt("highscore", 0);
         highscoreTextView.setText(String.valueOf(highscore));
+        scoreTextView.setText(String.valueOf(score));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         addValuesToDataSets(sensorEvent.values);
+        detectShake(sensorEvent.values);
+        setHighscore();
+        setTextViews();
     }
 
     @Override
@@ -103,12 +128,54 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    void detectShake(float[] values) {
+        float currentValue = values[0];
+        deltaX = Math.abs(lastX - currentValue);
+
+        // Set Shaking Status
+
+//        Log.d("MainActivity", "Delta: " + deltaX + " - IsShaking: " + isShaking);
+
+        if ((deltaX > SHAKE_INTESITY) || (isShaking && deltaX > 5)) {
+            isShaking = true;
+            score += deltaX;
+        } else if (isShaking && deltaX < SHAKE_INTESITY) {
+            isShaking = false;
+            score = 0;
+        }
+
+        if (isShaking) {
+            Log.d("MainActivity", "Delta: " + deltaX + " - IsShaking: " + isShaking);
+        } else if (deltaX > 1) {
+            Log.e("MainActivity", "Delta: " + deltaX + " - IsShaking: " + isShaking);
+        }
+        lastX = currentValue;
+
+    }
+
+    void setHighscore() {
+        if (score > highscore) {
+            highscore = score;
+            highscoreTextView.setText(String.valueOf(highscore));
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("highscore", highscore);
+            editor.apply();
+        }
+    }
+
+    void resetHighscore() {
+        highscore = 0;
+        highscoreTextView.setText(String.valueOf(highscore));
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("highscore", highscore);
+        editor.apply();
+    }
+
+    // Add values to chart
     void addValuesToDataSets(float[] values) {
         time++;
 
         xAxisLineData.addEntry(new Entry(time, values[0]));
-//        yAxisLineData.addEntry(new Entry(time, values[1]));
-//        zAxisLineData.addEntry(new Entry(time, values[2]));
 
         if(xAxisLineData.getEntryCount() >= CHART_ENTRIES_LIMIT) {
             xAxisLineData.removeFirst();
@@ -118,6 +185,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         dataSets.add(xAxisLineData);
         LineData lineData = new LineData(dataSets);
         chart.setData(lineData);
-        chart.invalidate(); // refresh
+        chart.invalidate();
+    }
+
+    // Set TextViews
+    void setTextViews() {
+        if(isShaking) {
+            shakeTextView.setText(getString(R.string.shaking));
+            shakeTextView.setTextColor(getResources().getColor(R.color.green));
+        } else {
+            shakeTextView.setText(getString(R.string.not_shaking));
+            shakeTextView.setTextColor(getResources().getColor(R.color.red));
+        }
+
+        if (isShaking) {
+            scoreTextView.setText(String.valueOf(score));
+        }
+        highscoreTextView.setText(String.valueOf(highscore));
     }
 }
